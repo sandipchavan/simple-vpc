994f6f9e001129893f7abb3b6774e724986236a2
S Chavan <sandipchavan1994@gmail.com>
2021-08-30 22:44:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 provider "aws" {
+  profile = "ctg-devops-demo"
   region = local.region
 }
 
@@ -63,7 +64,7 @@ module "ec2_instance" {
 
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = "t2.micro"
-  key_name               = "user1"
+  key_name               = "terraform-demo"
   monitoring             = true
   #vpc_security_group_ids = ["sg-12345678"]
   subnet_id              =  module.vpc.public_subnets[0]
@@ -74,3 +75,81 @@ module "ec2_instance" {
   }
    depends_on = [module.vpc]
 }
+
+
+module "db" {
+  source  = "terraform-aws-modules/rds/aws"
+  version = "~> 3.0"
+
+  identifier = "demodb"
+
+  engine            = "mysql"
+  engine_version    = "5.7.19"
+  instance_class    = "db.t2.large"
+  allocated_storage = 5
+
+  name     = "demodb"
+  username = "terraform"
+  password = "Piya1234"
+  port     = "3306"
+
+  iam_database_authentication_enabled = true
+
+  //vpc_security_group_ids = ["sg-12345678"]
+
+  //maintenance_window = "Mon:00:00-Mon:03:00"
+  //backup_window      = "03:00-06:00"
+
+  # Enhanced Monitoring - see example for details on how to create the role
+  # by yourself, in case you don't want to create it automatically
+  monitoring_interval = "30"
+  monitoring_role_name = "MyRDSMonitoringRole"
+  create_monitoring_role = true
+
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+
+  # DB subnet group
+  subnet_ids = [ module.vpc.private_subnets[0], module.vpc.private_subnets[1] ]
+
+  # DB parameter group
+  family = "mysql5.7"
+
+  # DB option group
+  major_engine_version = "5.7"
+
+  # Database Deletion Protection
+  deletion_protection = false
+
+  parameters = [
+    {
+      name = "character_set_client"
+      value = "utf8mb4"
+    },
+    {
+      name = "character_set_server"
+      value = "utf8mb4"
+    }
+  ]
+
+  options = [
+    {
+      option_name = "MARIADB_AUDIT_PLUGIN"
+
+      option_settings = [
+        {
+          name  = "SERVER_AUDIT_EVENTS"
+          value = "CONNECT"
+        },
+        {
+          name  = "SERVER_AUDIT_FILE_ROTATIONS"
+          value = "37"
+        },
+      ]
+    },
+  ]
+  depends_on = [module.vpc]
+}
+
